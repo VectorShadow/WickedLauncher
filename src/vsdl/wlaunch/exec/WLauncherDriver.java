339cec0cd2ad592370e0ff56094cb76c49fc4e46
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-08 20:47:30 -0500

    TextImager overhaul to make full use of font metrics for dynamic sizing.

--- src/vsdl/wlaunch/exec/WLauncherDriver.java
+++ src/vsdl/wlaunch/exec/WLauncherDriver.java
@@ -16,12 +16,68 @@ public class WLauncherDriver {
         try {
             getDataLink().transmit(new DataMessage("{Hello world!}"));
         } catch (ConnectException e) {
-            getGui().paintCanvas(TextImager.fromString("FATAL ERROR: Unable to contact game server!", 40, 68, 2048, Color.BLACK, Color.RED) , new Point(0,0), Color.BLACK.getRGB());
+            getGui().paintCanvas(
+                    TextImager.image(
+                            "FATAL ERROR: Unable to contact game server!",
+                            36,
+                            72,
+                            400,
+                            Color.BLACK,
+                            Color.RED
+                    ),
+                    new Point(0,0),
+                    0
+            );
         }
-//        getGui().paintCanvas(TextImager.fromChar('?', new Font(Font.SERIF, Font.ITALIC,256), Color.GREEN, Color.BLACK, true), new Point(0,0), Color.BLACK.getRGB());
-//        getGui().paintCanvas(TextImager.fromChar('?', new Font(Font.SERIF, Font.ITALIC,256), Color.RED, Color.BLACK, false), new Point(0,256), Color.BLACK.getRGB());
-//        getGui().paintCanvas(TextImager.fromString("Test text string of many chars exceeding width", new Font(Font.SERIF, Font.ITALIC,12), 135, 900, Color. WHITE, Color.BLACK), new Point(0,0), Color.BLACK.getRGB());
-//        getGui().paintCanvas(TextImager.fromString("Overlap test text", new Font(Font.SERIF, Font.ITALIC,12), 135, 655, Color.RED, Color.BLACK), new Point(100,15), Color.BLACK.getRGB());
+        getGui().paintCanvas(
+            TextImager.image("\tMultiple Word Test? Multiple line test with special\t characters\n test.", 25, 100, 400, Color.WHITE, Color.BLACK),
+                    new Point(100, 100),
+                   0);
+        getGui().paintCanvas(
+                TextImager.imageAsTile(' ', 264, Color.BLACK, Color.LIGHT_GRAY),
+                new Point(500, 50),
+                0
+        );
+        getGui().paintCanvas(
+                TextImager.imageAsTile('@', 128, Color.WHITE, Color.BLACK),
+                new Point(504, 54),
+                0x00000000
+        );
+        getGui().paintCanvas(
+                TextImager.imageAsTile('D', 128, Color.GREEN, Color.BLACK),
+                new Point(632, 54),
+                0x00000000
+        );
+        getGui().paintCanvas(
+                TextImager.imageAsTile('T', 128, Color.DARK_GRAY, Color.BLACK),
+                new Point(504, 182),
+                0x00000000
+        );
+        getGui().paintCanvas(
+                TextImager.imageAsTile('$', 128, Color.GREEN, Color.YELLOW),
+                new Point(632, 182),
+                0x00000000
+        );
+        getGui().paintCanvas(
+                TextImager.image("[PlayerName]", 25, 25, 250, Color.GREEN, Color.BLACK),
+                new Point(512, 68),
+                0xff000000
+        );
+        getGui().paintCanvas(
+                TextImager.image("Green Dragon <Hostile>", 25, 25, 250, Color.RED, Color.BLACK),
+                new Point(625, 68),
+                0xff000000
+        );
+        getGui().paintCanvas(
+                TextImager.image("Stone Troll <Asleep>", 25, 25, 250, Color.ORANGE, Color.BLACK),
+                new Point(495, 195),
+                0xff000000
+        );
+        getGui().paintCanvas(
+                TextImager.image("A Pile of Coins", 25, 25, 250, Color.MAGENTA, Color.BLACK),
+                new Point(625, 195),
+                0xff000000
+        );
         getGui().updateFrameImage();
     }
 }
