e90a23ce1511e27e2657b2834e9cbe57955e01a2
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-17 11:13:47 -0500

    More key exchange updates.

--- src/vsdl/wlaunch/connections/WLauncherDataMessageHandler.java
+++ src/vsdl/wlaunch/connections/WLauncherDataMessageHandler.java
@@ -1,6 +1,9 @@
 package vsdl.wlaunch.connections;
 
 import vsdl.datavector.api.DataMessageHandler;
+import vsdl.datavector.crypto.CryptoUtilities;
+import vsdl.datavector.crypto.Encryption;
+import vsdl.datavector.crypto.RSA;
 import vsdl.datavector.elements.DataMessage;
 import vsdl.datavector.elements.DataMessageBuilder;
 import vsdl.datavector.link.DataLink;
@@ -8,6 +11,7 @@ import vsdl.wlaunch.exec.WLauncherEntityManager;
 
 import static vsdl.wl.elements.DataMessageHeaders.*;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class WLauncherDataMessageHandler implements DataMessageHandler {
@@ -18,8 +22,17 @@ public class WLauncherDataMessageHandler implements DataMessageHandler {
         String header = blocks.get(0);
         switch (header) {
             case PUBLIC_KEY:
+                System.out.println(WLauncherEntityManager.getSessionKey());
                 dataLink.transmit(
-                        DataMessageBuilder.start(SESSION_KEY).addBlock(WLauncherEntityManager.getSessionKey()).build()
+                        DataMessageBuilder
+                                .start(SESSION_KEY)
+                                .addBlock(
+                                        RSA.encrypt(
+                                                new BigInteger(WLauncherEntityManager.getSessionKey(), Character.MAX_RADIX),
+                                                new BigInteger(blocks.get(1), Character.MAX_RADIX)
+                                        ).toString(Character.MAX_RADIX)
+                                )
+                                .build()
                 );
                 break;
             default:
