2a113f7509a72079ab9a0cc518819367b5ee1fd1
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-18 10:17:07 -0500

    Added SimpleInputDialogInputSource.

--- src/vsdl/wlaunch/exec/WLauncherDriver.java
+++ src/vsdl/wlaunch/exec/WLauncherDriver.java
@@ -7,11 +7,13 @@ import vsdl.wlaunch.ui.Terminal;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
 
 import static vsdl.wlaunch.exec.WLauncherEntityManager.*;
 
 public class WLauncherDriver {
+
+    //todo - all special screens under a special controller
+    // builder classes for custom input sources
 
     private static void establishConnectionToRemoteHost() {
         try {
@@ -63,71 +65,5 @@ public class WLauncherDriver {
         getTerminal().setImageContextProfile(ImageContextProfileBuilder.start().build());
         getTerminal().start();
         establishConnectionToRemoteHost();
-//        try {
-//            getDataLink().transmit(new DataMessage("{Hello world!}"));
-//        } catch (ConnectException e) {
-//            getGui().paintCanvas(
-//                    TextImager.image(
-//                            "FATAL ERROR: Unable to contact game server!",
-//                            36,
-//                            72,
-//                            400,
-//                            Color.BLACK,
-//                            Color.RED
-//                    ),
-//                    new Point(0,0),
-//                    0
-//            );
-//        }
-//        getGui().paintCanvas(
-//            TextImager.image("\tMultiple Word Test? Multiple line test with special\t characters\n test.", 25, 100, 400, Color.WHITE, Color.BLACK),
-//                    new Point(100, 100),
-//                   0);
-//        getGui().paintCanvas(
-//                TextImager.imageAsTile(' ', 264, Color.BLACK, Color.LIGHT_GRAY),
-//                new Point(500, 50),
-//                0
-//        );
-//        getGui().paintCanvas(
-//                TextImager.imageAsTile('@', 128, Color.WHITE, Color.BLACK),
-//                new Point(504, 54),
-//                0x00000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.imageAsTile('D', 128, Color.GREEN, Color.BLACK),
-//                new Point(632, 54),
-//                0x00000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.imageAsTile('T', 128, Color.DARK_GRAY, Color.BLACK),
-//                new Point(504, 182),
-//                0x00000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.imageAsTile('$', 128, Color.GREEN, Color.YELLOW),
-//                new Point(632, 182),
-//                0x00000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.image("[PlayerName]", 25, 25, 250, Color.GREEN, Color.BLACK),
-//                new Point(512, 68),
-//                0xff000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.image("Green Dragon <Hostile>", 25, 25, 250, Color.RED, Color.BLACK),
-//                new Point(625, 68),
-//                0xff000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.image("Stone Troll <Asleep>", 25, 25, 250, Color.ORANGE, Color.BLACK),
-//                new Point(495, 195),
-//                0xff000000
-//        );
-//        getGui().paintCanvas(
-//                TextImager.image("A Pile of Coins", 25, 25, 250, Color.MAGENTA, Color.BLACK),
-//                new Point(625, 195),
-//                0xff000000
-//        );
-//        getGui().updateFrameImage();
     }
 }
