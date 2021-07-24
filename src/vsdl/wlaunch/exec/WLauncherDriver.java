c17809a40f594f7d93375aa6f82e9818c8c91a88
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-24 13:17:38 -0500

    Get uppermost context added to ICH. KeyPresses that fail to find a context now attempt to apply to the uppermost. Lots of updates to support FieldEntryTextDialogImageSource. (Still WIP) Added InputEventUtils class. Added default font name and style to TextImager.

--- src/vsdl/wlaunch/exec/WLauncherDriver.java
+++ src/vsdl/wlaunch/exec/WLauncherDriver.java
@@ -2,6 +2,8 @@ package vsdl.wlaunch.exec;
 
 import vsdl.omnigui.image.context.ImageContext;
 import vsdl.omnigui.image.context.ImageContextProfileBuilder;
+import vsdl.omnigui.image.source.FieldEntryTextDialogImageSource;
+import vsdl.omnigui.image.source.InteractiveTextDialogImageSource;
 import vsdl.omnigui.image.source.ScrollableMenuTextDialogImageSource;
 import vsdl.wlaunch.ui.Terminal;
 
@@ -21,54 +23,94 @@ public class WLauncherDriver {
             System.out.println("Connected successfully!");
             //todo - next step once connected!
         } catch (IllegalStateException e) {
-            ScrollableMenuTextDialogImageSource connectionFailureMenu =
-                    ScrollableMenuTextDialogImageSource.builder()
+            FieldEntryTextDialogImageSource userNameAndPasswordInputMenu =
+                    FieldEntryTextDialogImageSource.builder()
                     .setColors(
                             Color.BLACK,
                             Color.RED,
                             Color.LIGHT_GRAY,
                             Color.WHITE,
-                            Color.DARK_GRAY,
                             Color.BLACK,
                             Color.BLACK,
-                            Color.BLACK
+                            Color.BLUE,
+                            Color.WHITE
                     )
                     .setTextHeight(36)
-                    .setTitle("Unable to contact game server!")
-                    .setOptionCount(3)
-                    .setOptionNames(
-                            "Retry",
-                            "Enter alternate host name",
-                            "Exit")
-                    .setOptionDescriptions(
-                            "Continue attempting to contact the server",
-                            "Manually enter a game server host-name",
-                            "Exit the game client"
-                    )
-                    .setOptionEnabledState(false, 1)
-                    .setOptionExecutions(
-                            WLauncherDriver::establishConnectionToRemoteHost,
-                            () -> {}, //todo - a prompt to update WLauncherEntityManager.HOST
-                            () -> System.exit(0)
-                    )
-                    .setEscapeExecution(
-                            () -> System.exit(0)
-                    )
+                    .setTitle("Enter Username and Password:")
+                            .setOptionCount(3)
+                            .setOptionNames(
+                                    "Username: ",
+                                    "Password: ",
+                                    "Confirm: "
+                            )
+                            .setInputFieldMaskState(true, 1)
+                            .setInputFieldMaskState(true, 2)
+                            .setInputFieldCharacterLimits(
+                                    8,
+                                    8,
+                                    8
+                            )
+                    .setEscapeExecution(() -> System.exit(0))
                     .build();
+
             getTerminal().setImageContextProfile(
                     ImageContextProfileBuilder.start().appendImageContext(
                             new ImageContext(
-                                    connectionFailureMenu,
-                                    connectionFailureMenu.getOrigin(Terminal.CANVAS_DIM)
-                            ),
-                            KeyEvent.VK_ENTER,
-                            KeyEvent.VK_UP,
-                            KeyEvent.VK_DOWN,
-                            KeyEvent.VK_NUMPAD8,
-                            KeyEvent.VK_NUMPAD2,
-                            KeyEvent.VK_ESCAPE
+                                    userNameAndPasswordInputMenu,
+                                    userNameAndPasswordInputMenu.getOrigin(Terminal.CANVAS_DIM)
+                            )
                     ).build()
             );
+//
+//
+//            ScrollableMenuTextDialogImageSource connectionFailureMenu =
+//                    ScrollableMenuTextDialogImageSource.builder()
+//                    .setColors(
+//                            Color.BLACK,
+//                            Color.RED,
+//                            Color.LIGHT_GRAY,
+//                            Color.WHITE,
+//                            Color.DARK_GRAY,
+//                            Color.BLACK,
+//                            Color.BLACK,
+//                            Color.BLACK
+//                    )
+//                    .setTextHeight(36)
+//                    .setTitle("Unable to contact game server!")
+//                    .setOptionCount(3)
+//                    .setOptionNames(
+//                            "Retry",
+//                            "Enter alternate host name",
+//                            "Exit")
+//                    .setOptionDescriptions(
+//                            "Continue attempting to contact the server",
+//                            "Manually enter a game server host-name",
+//                            "Exit the game client"
+//                    )
+//                    .setOptionEnabledState(false, 1)
+//                    .setOptionExecutions(
+//                            WLauncherDriver::establishConnectionToRemoteHost,
+//                            () -> {}, //todo - a prompt to update WLauncherEntityManager.HOST
+//                            () -> System.exit(0)
+//                    )
+//                    .setEscapeExecution(
+//                            () -> System.exit(0)
+//                    )
+//                    .build();
+//            getTerminal().setImageContextProfile(
+//                    ImageContextProfileBuilder.start().appendImageContext(
+//                            new ImageContext(
+//                                    connectionFailureMenu,
+//                                    connectionFailureMenu.getOrigin(Terminal.CANVAS_DIM)
+//                            ),
+//                            KeyEvent.VK_ENTER,
+//                            KeyEvent.VK_UP,
+//                            KeyEvent.VK_DOWN,
+//                            KeyEvent.VK_NUMPAD8,
+//                            KeyEvent.VK_NUMPAD2,
+//                            KeyEvent.VK_ESCAPE
+//                    ).build()
+//            );
         }
     }
 
